6904a8b6889abba89a343beba2ff7f86941cf920
Mohd-Shadav <muhammadshadav166@gmail.com>
2025-12-13 15:55:59 +0530

    commit test

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,16 @@ import Login from './components/Auth/Login.jsx'
 import TrainersPage from './components/Trainers/TrainersPage.jsx'
 import StudentPage from './components/StudentComponents/StudentPage.jsx'
 
+
 function App() {
   return (
     <div>
     {/* <Login/> */}
-    <TrainersPage/>
+    {/* <TrainersPage/> */}
     {/* <StudentPage/> */}
+   
+
+    
     </div>
   )
 }
